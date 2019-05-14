f8c54f4720d4883979d05b8c4f1014366d10087d
cjyiz <cjyiz@aliyun.com>
2019-05-14 23:48:45 +0800

    '使用props传值按钮类型'

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12414,7 +12414,15 @@ exports.default = void 0;
 //
 //
 //
-var _default = {};
+//
+//
+//
+//
+//
+//
+var _default = {
+  props: ['icon']
+};
 exports.default = _default;
         var $3ba049 = exports.default || module.exports;
       
@@ -12428,7 +12436,18 @@ exports.default = _default;
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { staticClass: "button" }, [_vm._v("按钮")])
+  return _c(
+    "button",
+    { staticClass: "button" },
+    [
+      _c("svg", { staticClass: "icon", attrs: { "aria-hidden": "true" } }, [
+        _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
+      ]),
+      _vm._v(" "),
+      _vm._t("default")
+    ],
+    2
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12506,7 +12525,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55482" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49872" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/button.vue
+++ src/button.vue
@@ -1,16 +1,21 @@
 <template>
-  <button class='button'>按钮</button>
+  <button class="button">
+    <svg class="icon" aria-hidden="true">
+      <!-- 使用v-bind绑定样式 -->
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <slot></slot>
+  </button>
 </template>
 <script>
 export default {
- 
-}
+  props: ['icon']
+};
 </script>
 <style>
-.button{
+.button {
   width: 200px;
   height: 200px;
-  background-color: red;;
 }
 </style>
 
