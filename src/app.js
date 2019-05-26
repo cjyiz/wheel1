fb0298dfe431ec564e7df74ca7381ad23554a77c
cjyiz <cjyiz@aliyun.com>
2019-05-26 10:14:50 +0800

    '添加按钮组及检测组内容是否都是button'

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
-import button from './button'
+import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 // Vue.config.productionTip = false
-Vue.component('g-button',button)
+Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
+Vue.component('g-button-group',ButtonGroup)
 new Vue({
   el:'#app',
   data:{
---
+++ src/button-group.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+    mounted(){
+        // console.log(this.$el.children)
+        // 打印出div元素
+        for(let node of this.$el.children){
+            let name=node.nodeName.toLowerCase()
+            if( name !=='button'){
+                console.warn(`g-button-group的子元素应该全是button,但是你写的是${name}`)
+            }
+            console.log(node)
+        }
+    }
+};
+</script>
+<style lang='scss'>
+.g-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  > .g-button {
+    border-radius: 0;
+    margin-left:-1px;
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover{
+        position:relative;
+        z-index: 1;
+    }
+  }
+}
+</style>
