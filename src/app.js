b9c3d3ef3e7d5afe2b37a65fddf4e74c8d160210
cjyiz <cjyiz@aliyun.com>
2019-05-26 18:32:38 +0800

    '用karma自动化测试，并在button内全局注册icon'

--- src/app.js
+++ src/app.js
@@ -15,107 +15,107 @@ new Vue({
   }
 })
 
-// 单元测试
-import  chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-const expect=chai.expect
-// 一个单元测试用例
-{
-  const Constructor = Vue.extend(Button)
-  const button=new Constructor(
-    {
-      propsData:{
-        icon:'shezhi',
-      }
-    }
-  )
-  button.$mount('#test')
-  let useElement =button.$el.querySelector('use')
-  let href=useElement.getAttribute('xlink:href')
-  // 测试语法，固定的
-  expect(href).to.eq('#i-shezhi')
-  button.$el.remove()
-  button.$destroy()
-}
-// 第二个单元测试用例
-{
-  const Constructor = Vue.extend(Button)
-  const button=new Constructor(
-    {
-      propsData:{
-        icon:'shezhi',
-        loading:true
-      }
-    }
-  )
-  button.$mount('#test2')
-  let useElement =button.$el.querySelector('use')
-  let href=useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-loading')
-  button.$el.remove()
-  button.$destroy()
-}
+// // 单元测试
+// import  chai from 'chai'
+// import spies from 'chai-spies'
+// chai.use(spies)
+// const expect=chai.expect
+// // 一个单元测试用例
+// {
+//   const Constructor = Vue.extend(Button)
+//   const button=new Constructor(
+//     {
+//       propsData:{
+//         icon:'shezhi',
+//       }
+//     }
+//   )
+//   button.$mount('#test')
+//   let useElement =button.$el.querySelector('use')
+//   let href=useElement.getAttribute('xlink:href')
+//   // 测试语法，固定的
+//   expect(href).to.eq('#i-shezhi')
+//   button.$el.remove()
+//   button.$destroy()
+// }
+// // 第二个单元测试用例
+// {
+//   const Constructor = Vue.extend(Button)
+//   const button=new Constructor(
+//     {
+//       propsData:{
+//         icon:'shezhi',
+//         loading:true
+//       }
+//     }
+//   )
+//   button.$mount('#test2')
+//   let useElement =button.$el.querySelector('use')
+//   let href=useElement.getAttribute('xlink:href')
+//   expect(href).to.eq('#i-loading')
+//   button.$el.remove()
+//   button.$destroy()
+// }
 
-{
-  const div=document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const button=new Constructor(
-    {
-      propsData:{
-        icon:'shezhi',
-        loading:true
-      }
-    }
-  )
-  button.$mount(div)
-  let svg =button.$el.querySelector('svg')
-  let {order}=window.getComputedStyle(svg)
-  expect(order).to.eq('1')
-  // 结束后把用例删除,r如果没有成功，则后面不执行
-  button.$el.remove()
-  button.$destroy()
-}
+// {
+//   const div=document.createElement('div')
+//   document.body.appendChild(div)
+//   const Constructor = Vue.extend(Button)
+//   const button=new Constructor(
+//     {
+//       propsData:{
+//         icon:'shezhi',
+//         loading:true
+//       }
+//     }
+//   )
+//   button.$mount(div)
+//   let svg =button.$el.querySelector('svg')
+//   let {order}=window.getComputedStyle(svg)
+//   expect(order).to.eq('1')
+//   // 结束后把用例删除,r如果没有成功，则后面不执行
+//   button.$el.remove()
+//   button.$destroy()
+// }
 
-{
-  const div=document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const button=new Constructor(
-    {
-      propsData:{
-        icon:'shezhi',
-        iconPosition:'right'
-      }
-    }
-  )
-  button.$mount(div)
-  let svg =button.$el.querySelector('svg')
-  let {order}=window.getComputedStyle(svg)
-  expect(order).to.eq('2')
-  button.$el.remove()
-  button.$destroy()
-}
+// {
+//   const div=document.createElement('div')
+//   document.body.appendChild(div)
+//   const Constructor = Vue.extend(Button)
+//   const button=new Constructor(
+//     {
+//       propsData:{
+//         icon:'shezhi',
+//         iconPosition:'right'
+//       }
+//     }
+//   )
+//   button.$mount(div)
+//   let svg =button.$el.querySelector('svg')
+//   let {order}=window.getComputedStyle(svg)
+//   expect(order).to.eq('2')
+//   button.$el.remove()
+//   button.$destroy()
+// }
 
-{
-  // mock测试用例
-  // 使用chai-spies监听函数
-  const div=document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm=new Constructor(
-    {
-      propsData:{
-        icon:'shezhi'
-      }
-    }
-  )
-  vm.$mount(div)
-  let spy=chai.spy(function(){})
-  vm.$on('click',spy)
-  // 希望这个函数被执行
-  let button =vm.$el
-  button.click()
-  expect(spy).to.have.been.called()
-}
+// {
+//   // mock测试用例
+//   // 使用chai-spies监听函数
+//   const div=document.createElement('div')
+//   document.body.appendChild(div)
+//   const Constructor = Vue.extend(Button)
+//   const vm=new Constructor(
+//     {
+//       propsData:{
+//         icon:'shezhi'
+//       }
+//     }
+//   )
+//   vm.$mount(div)
+//   let spy=chai.spy(function(){})
+//   vm.$on('click',spy)
+//   // 希望这个函数被执行
+//   let button =vm.$el
+//   button.click()
+//   expect(spy).to.have.been.called()
+// }
