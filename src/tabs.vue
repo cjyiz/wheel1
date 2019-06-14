e3f4f40dc5e173cf86aabc7b4fd7ae7d606df3da
cjyiz <cjyiz@aliyun.com>
2019-06-14 22:18:41 +0800

    '使用eventbus传值'

--- src/tabs.vue
+++ src/tabs.vue
@@ -1,31 +1,61 @@
 <template>
-    <div class='tabs'>
-        <slot></slot>    
-    </div>
+  <div class="tabs">
+    <slot></slot>
+  </div>
 </template>
 <script>
 export default {
-    name:'GuluTabs',
-    props:{
-        selected:{
-            type:String,
-            required:true
-        },
-        direction:{
-            type:String,
-            default:'horizontal',
-            validator(value){
-                return  ['horizontal','vertical'].indexOf(value)>=0
-            }
-        }
+  name: "GuluTabs",
+  props: {
+    selected: {
+      type: String,
+      required: true
     },
-    created(){
-        this.$emit()
+    direction: {
+      type: String,
+      default: "horizontal",
+      validator(value) {
+        return ["horizontal", "vertical"].indexOf(value) >= 0;
+      }
     }
-}
+  },
+  data() {
+    return {
+      eventBus: new Vue()
+    };
+  },
+  provide() {
+    return {
+      eventBus: this.eventBus
+    };
+  },
+  mounted() {
+    if (this.$children.length === 0) {
+      console &&
+        console.warn &&
+        console.warn(
+          "tabs的子组件应该是tabs-head和tabs-nav，但是你没有写子组件"
+        );
+    }
+    this.$children.forEach(vm => {
+      if (vm.$options.name === "GuluTabsHead") {
+        vm.$children.forEach(childVm => {
+          if (
+            childVm.$options.name === "GuluTabsItem" &&
+            childVm.name === this.selected
+          ) {
+            this.eventBus.$emit("update:selected", this.selected, childVm);
+          }
+        });
+      }
+    });
+  },
+  created() {
+    this.$emit();
+  }
+};
 </script>
 <style>
-
 </style>
 
 
